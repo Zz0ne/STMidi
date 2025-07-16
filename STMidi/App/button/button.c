9a1936fcc1bdc4c024d69b48c3db685bd3f0352a
nuno <nunotechwork@gmail.com>
2025-07-17 00:06:09 +0100

    Continue button library development

--- STMidi/App/button/button.c
+++ STMidi/App/button/button.c
@@ -7,18 +7,37 @@ button_handle_t *_buttons[MAX_BUTTONS] = {0};
 
 void button_init(button_handle_t *btn)
 {
-    for (button_event_type_t event = 0; event < BUTTON_EVENT_COUNT; event++)
+    GPIO_InitTypeDef gpio = {0};
+    gpio.Pin              = btn->pin;
+    gpio.Mode             = GPIO_MODE_IT_RISING_FALLING;
+    gpio.Pull             = GPIO_PULLUP;
+    HAL_GPIO_Init(btn->port, &gpio);
+
+    for (button_event_t event = 0; event < BUTTON_EVENT_COUNT; event++)
     {
         btn->_events[event].type     = event;
         btn->_events[event].callback = NULL;
     }
 }
 
-void button_register_event(button_handle_t *btn, button_event_type_t type, button_callback_t cb)
+void button_register_event(button_handle_t *btn, button_event_t type, button_callback_t cb)
 {
-    for (button_event_type_t event = 0; event < BUTTON_EVENT_COUNT; event++)
+    for (button_event_t event = 0; event < BUTTON_EVENT_COUNT; event++)
         if (btn->_events[event].type == type)
             btn->_events[event].callback = cb;
+}
+
+void button_unregister_event(button_handle_t *btn, button_event_t type)
+{
+    for (button_event_t event = 0; event < BUTTON_EVENT_COUNT; event++)
+        if (btn->_events[event].type == type)
+            btn->_events[event].callback = NULL;
+}
+
+void button_unregister_all(button_handle_t *btn)
+{
+    for (button_event_t event = 0; event < BUTTON_EVENT_COUNT; event++)
+        btn->_events[event].callback = NULL;
 }
 
 void button_poll(void)
--- STMidi/App/button/button.h
+++ STMidi/App/button/button.h
@@ -9,24 +9,25 @@ typedef enum
     BUTTON_EVENT_RELEASE,
     BUTTON_EVENT_LONG_PRESS,
     BUTTON_EVENT_DOUBLE_PRESS
-} button_event_type_t;
+} button_event_t;
 
 typedef void (*button_callback_t)(void);
 
 typedef struct
 {
-    button_event_type_t type;
-    button_callback_t callback;
-} button_event_t;
-
-typedef struct
-{
     GPIO_TypeDef *port;
     uint16_t pin;
-    button_event_t _events[BUTTON_EVENT_COUNT];
-    uint8_t _events_registed_count;
+
+    struct
+    {
+        button_event_t type;
+        button_callback_t callback;
+    } _events[BUTTON_EVENT_COUNT];
+
 } button_handle_t;
 
 void button_init(button_handle_t *btn);
-void button_register_event(button_handle_t *btn, button_event_type_t type, button_callback_t cb);
+void button_register_event(button_handle_t *btn, button_event_t type, button_callback_t cb);
+void button_unregister_event(button_handle_t *btn, button_event_t type);
+void button_unregister_all(button_handle_t *btn);
 void button_poll(void);
